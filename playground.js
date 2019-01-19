de0680940f5a1303ba331bcd62153402799fd897
04an <olegdzhum@gmail.com>
2019-01-19 14:42:05 +0100

    Add JS

---
+++ playground.js
@@ -0,0 +1,4 @@
+console.log("oleg dzhum".toUpperCase() );
+console.log("JavaScript jest mocno typowanym językiem".replace("mocno","słabo"));
+console.log( "JavaScript jest językiem wysokogo poziomu".split(' ') );
+console.log( "Jestem królem fromnt-endu!".repeat(3));
